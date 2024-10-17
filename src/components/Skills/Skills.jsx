5c294f35cc4ad8c48bb273f4a5d44648cd570c29
Shreekant <shree.kant.74.sk@gmail.com>
2024-10-17 10:23:54 +0530

    skills page updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Skills = () => {
   const location = useLocation();
@@ -9,53 +9,195 @@ const Skills = () => {
       data-page="resume"
     >
       <header>
-        <h2 className="h2 article-title">Resume</h2>
+        <h2 className="h2 article-title">Skills</h2>
       </header>
       {/* Skills Section */}
       <section className="skill" style={{ marginBottom: "20px" }}>
-        <h3 className="h3 skills-title">My skills</h3>
+        <h3 className="h3 skills-title">Languages and Tools:</h3>
         <ul className="skills-list content-card">
           <li className="skills-item">
             <div className="title-wrapper">
-              <img
-                src="https://via.placeholder.com/40/0000FF/FFFFFF?text=Image"
-                alt="Placeholder Image"
-              />
-
-              <h5 className="h5">Web design</h5>
+              <a
+                href="https://www.w3.org/html/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                  alt="html5"
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
           </li>
           <li className="skills-item">
             <div className="title-wrapper">
-              <img
-                src="https://via.placeholder.com/40/0000FF/FFFFFF?text=Image"
-                alt="Placeholder Image"
-              />
-
-              <h5 className="h5">Web design</h5>
+              <a
+                href="https://www.w3schools.com/css/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                  alt="css3"
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
           </li>
           <li className="skills-item">
             <div className="title-wrapper">
-              <img
-                src="https://via.placeholder.com/40/0000FF/FFFFFF?text=Image"
-                alt="Placeholder Image"
-              />
-
-              <h5 className="h5">Web design</h5>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                  alt="javascript"
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
           </li>
           <li className="skills-item">
             <div className="title-wrapper">
-              <img
-                src="https://via.placeholder.com/40/0000FF/FFFFFF?text=Image"
-                alt="Placeholder Image"
-              />
-
-              <h5 className="h5">Web design</h5>
+              <a
+                href="https://www.chartjs.org"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://www.chartjs.org/media/logo-title.svg"
+                  alt="chartjs"
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
           </li>
-
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+                  alt="react"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a href="https://redux.js.org" target="_blank" rel="noreferrer">
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+                  alt="redux"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+                  alt="tailwind"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-plain-wordmark.svg"
+                  alt="nodejs"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+                <img
+                  src="https://github.com/user-attachments/assets/f5207c10-b402-4450-ac92-0df2d9f351f2"
+                  height="40"
+                  width="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a href="https://postman.com" target="_blank" rel="noreferrer">
+                <img
+                  src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+                  alt="postman"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a
+                href="https://www.mongodb.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-plain-wordmark.svg"
+                  alt="mongodb"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+                <img
+                  src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+                  alt="git"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
+          <li className="skills-item">
+            <div className="title-wrapper">
+              <a
+                href="https://www.photoshop.com/en"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/photoshop/photoshop-original.svg"
+                  alt="photoshop"
+                  width="40"
+                  height="40"
+                />
+              </a>
+            </div>
+          </li>
         </ul>
       </section>
 
@@ -70,34 +212,79 @@ const Skills = () => {
 
         <ol className="timeline-list">
           <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">Creative director</h4>
-            <span>2015 — Present</span>
+            <h4 className="h4 timeline-item-title">Web Development intern</h4>
+            <span>Aug 2024 — Present</span>
             <p className="timeline-text">
-              Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit
-              atque corrupti, quos dolores et quas molestias exceptur.
-            </p>
-          </li>
-
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">Art director</h4>
-            <span>2013 — 2015</span>
-            <p className="timeline-text">
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
-              delenit atque corrupti, quos dolores et quas molestias exceptur.
-            </p>
-          </li>
-
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">Web designer</h4>
-            <span>2010 — 2013</span>
-            <p className="timeline-text">
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
-              delenit atque corrupti, quos dolores et quas molestias exceptur.
+              As a Web Development Intern at Upskill, you worked on building
+              responsive websites, improving user experience, and collaborating
+              with senior developers to optimize performance, gaining hands-on
+              experience in front-end and back-end development.
             </p>
           </li>
         </ol>
       </section>
 
+      {/* Certification */}
+      <section className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
+            <ion-icon name="book-outline"></ion-icon>
+          </div>
+          <h3 className="h3">Certifications</h3>
+        </div>
+
+        <ol className="timeline-list">
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">
+              Front-end Development-libraries
+            </h4>
+            <span>Issued Aug 2024</span>
+            <p className="timeline-text ">
+              <Link
+                to={
+                  "https://www.freecodecamp.org/certification/Shreekant_04/front-end-development-libraries"
+                }
+                target="_blank"
+                className="timeline-text-link"
+              >
+                Show Credential
+              </Link>
+            </p>
+          </li>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">
+              Javascript Algorithms and Data structures
+            </h4>
+            <span>Issued July 2024</span>
+            <p className="timeline-text">
+              <Link
+                to={
+                  "https://www.freecodecamp.org/certification/Shreekant_04/javascript-algorithms-and-data-structures-v8"
+                }
+                target="_blank"
+                className="timeline-text-link"
+              >
+                Show Credential
+              </Link>
+            </p>
+          </li>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Responsive Web Design</h4>
+            <span>Issued June 2024</span>
+            <p className="timeline-text">
+              <Link
+                to={
+                  "https://www.freecodecamp.org/certification/Shreekant_04/responsive-web-design"
+                }
+                target="_blank"
+                className="timeline-text-link"
+              >
+                Show Credential
+              </Link>
+            </p>
+          </li>
+        </ol>
+      </section>
       {/* Education Section */}
       <section className="timeline">
         <div className="title-wrapper">
@@ -110,33 +297,10 @@ const Skills = () => {
         <ol className="timeline-list">
           <li className="timeline-item">
             <h4 className="h4 timeline-item-title">
-              University school of the arts
+              St. Matthews Public School
             </h4>
-            <span>2007 — 2008</span>
-            <p className="timeline-text">
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
-              delenit atque corrupti, quos dolores et quas molestias exceptur.
-            </p>
-          </li>
-
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">New york academy of art</h4>
-            <span>2006 — 2007</span>
-            <p className="timeline-text">
-              Ratione voluptatem sequi nesciunt, facere quisquams facere menda
-              ossimus, omnis voluptas assumenda est omnis.
-            </p>
-          </li>
-
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">
-              High school of art and design
-            </h4>
-            <span>2002 — 2004</span>
-            <p className="timeline-text">
-              Duis aute irure dolor in reprehenderit in voluptate, quila
-              voluptas mag odit aut fugit, sed consequuntur magni dolores eos.
-            </p>
+            <span>2018 — 2020</span>
+            <p className="timeline-text">10+2, commerce</p>
           </li>
         </ol>
       </section>
